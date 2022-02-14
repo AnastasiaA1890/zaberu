94535b4ce5a1977b18572a60dfbffad15a93859d
Anastasia Andreeva <nastiy1890@yandex.ru>
2022-02-13 20:28:25 -0800

    feature: added new functionality popups

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,32 @@
-const popupOne = document.querySelector('.popup');
-const popupOneOpenButton = document.querySelector('.footer__link');
-const popupOneCloseButton = document.querySelector('.popup__close-button');
+//Terms variables
+const popupTerms = document.querySelector('#terms');
+const termsPopupOpenButton = document.querySelector('.footer__link-terms');
+const termsCloseButton = document.querySelector('#terms .popup__close-button')
+//Offer variables
+const popupOffer = document.querySelector('#offer');
+const offerPopupOpenButton = document.querySelector('.footer__link-offer');
+const offerCloseButton = document.querySelector('#offer .popup__close-button')
+//License variables
+const popupLicense = document.querySelector('#license');
+const licensePopupOpenButton = document.querySelector('.footer__link-license')
+const licenseCloseButton = document.querySelector('#license .popup__close-button')
 
-function openPopupOne() {
-    popupOne.classList.add('popup_opened');
+//OpenPopup function
+function openPopup(element) {
+    element.classList.add('popup_opened');
+}
+//ClosePopup function
+function closePopup(element) {
+    element.classList.remove('popup_opened');
 }
 
-function closePopupOne() {
-    popupOne.classList.remove('popup_opened');
-}
+//Event handlers
+//Open event handlers
+termsPopupOpenButton.addEventListener('click', () => openPopup(popupTerms));
+offerPopupOpenButton.addEventListener('click', () => openPopup(popupOffer));
+licensePopupOpenButton.addEventListener('click', () => openPopup(popupLicense));
+//Close event handlers
+termsCloseButton.addEventListener('click', () => closePopup(popupTerms));
+offerCloseButton.addEventListener('click', () => closePopup(popupOffer));
+licenseCloseButton.addEventListener('click', () => closePopup(popupLicense));
 
-popupOneOpenButton.addEventListener('click', openPopupOne);
-popupOneCloseButton.addEventListener('click', closePopupOne);
